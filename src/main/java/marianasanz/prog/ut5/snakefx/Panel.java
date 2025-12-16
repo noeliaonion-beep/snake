6cdb6757658ff6473f6bbfdde23f74812fc2023a
noeliaonion-beep <nferreicor@educacion.navarra.es>
2025-12-16 13:26:19 +0100

    Add files via upload

--- src/main/java/marianasanz/prog/ut5/snakefx/Panel.java
+++ src/main/java/marianasanz/prog/ut5/snakefx/Panel.java
@@ -1,21 +1,37 @@
 package marianasanz.prog.ut5.snakefx;
 
+import java.util.Scanner;
+
 public class Panel {
-    int[][] mapa;
-    int manzanas;
+
+  private int[][] mapa;
+  private int manzanas;
+  private int longitud;
+  private int posCabeza;
+
+
 
 
     public Panel() {
-        this.mapa =new int[8][8];
+        this.mapa =new int[9][9];
         this.manzanas = 64;
+        this.longitud=-3;
     }
 
 
     public void pintarSerpiente(){
+        int contador=0;
         //TODO Colocar la posicion de la serpiente dentro de la matriz ( CABEZA= -2 CUERPO= -1 COLA= -3)
         for (int lineas = 0; lineas <mapa.length ; lineas++) {
             for (int columnas = 0; columnas <mapa[0].length ; columnas++) {
-
+                if (lineas==mapa.length/2 && columnas==0){
+                    mapa[lineas][columnas]=longitud;
+                    longitud++;
+                    mapa[lineas][columnas+1]=longitud;
+                    longitud++;
+                    mapa[lineas][columnas+2]=longitud;
+                    posCabeza= mapa[lineas][columnas]=longitud;
+                }
             }
         }
     }
@@ -26,6 +42,9 @@ public class Panel {
 
     public void movimiento(){
         //TODO Capturar el movimiento de las flechas y que se mueva la serpiente dentro de la matriz segun la posicion
+        Scanner teclao = new Scanner(System.in);
+        String tecla= teclao.nextLine();
+
     }
 
     public void comer(){
@@ -36,6 +55,20 @@ public class Panel {
     }
     public void perder(){
         //TODO cuando la serpiente o se toque a si misma o se choque contra la pared pierde
+    }
+    public void mostrarMatriz(){
+        for (int i = 0; i <mapa.length ; i++) {
+            System.out.println();
+            for (int j = 0; j <mapa[0].length ; j++) {
+                System.out.printf("%3d",mapa[i][j]);
+            }
+        }
+    }
+
+    static void main() {
+        Panel panel=new Panel();
+        panel.pintarSerpiente();
+        panel.mostrarMatriz();
     }
 }
 
